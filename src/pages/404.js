11c35bbe5bf2b05dc77d0ba7e895eb53c0a146cc
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-05-07 20:08:38 -0600

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NotFoundPage = () => {
+
+  return (
+
+    <>
+      <h1> Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </>
+  )
+}
+export default NotFoundPage
