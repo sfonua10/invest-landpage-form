7b4f262b58ea0096dab22de1a144ec27d0aeef98
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-05-07 19:08:00 -0600

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../components/form'
+
+import "../global.css"
+
+const LandingPage = () => (
+  <>
+    <h1>Investments</h1>
+    <Form />
+  </>
+)
+
+export default LandingPage;
